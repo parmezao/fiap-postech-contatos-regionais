956d16b83d9b2cf2f72d85df8e68d63fb698f3ad
Wagner Cristiano Parmezão <wagner@parmex.com.br>
2025-03-14 18:43:34 -0300

    Add ContatosRegionais.Service project and BaseService class
    Updated the solution to include a new project `ContatosRegionais.Service` with a unique GUID.
    Created the project file targeting .NET 8.0, including implicit usings, nullable reference types,
    and references to `FluentValidation.AspNetCore` and `System.Text.Json`.
    
    Introduced the `BaseService<TEntity>` class to implement a generic service pattern for managing
    entities, providing asynchronous methods for CRUD operations using an injected repository.

---
+++ src/ContatosRegionais.Service/Services/BaseService.cs
@@ -0,0 +1,31 @@
+﻿using ContatosRegionais.Domain.Entities;
+using ContatosRegionais.Domain.Interfaces;
+
+namespace ContatosRegionais.Service.Services;
+
+public class BaseService<TEntity>(IBaseRepository<TEntity> baseRepository)
+    : IBaseService<TEntity> where TEntity : BaseEntity
+{
+    protected readonly IBaseRepository<TEntity> _baseRepository = baseRepository;
+
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await _baseRepository.InsertAsync(entity);
+        return entity;
+    }
+
+    public async Task DeleteAsync(int id) => await _baseRepository.DeleteAsync(id);
+
+    public async Task<IList<TEntity>> GetAllAsync() => await _baseRepository.SelectAllAsync();
+
+    public async Task<IEnumerable<TEntity>> FilterAsync(Func<TEntity, bool> predicate) =>
+        await _baseRepository.FilterAsync(predicate);
+
+    public async Task<TEntity> GetByIdAsync(int id) => (await _baseRepository.SelectAsync(id))!;
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        await _baseRepository.UpdateAsync(entity);
+        return entity;
+    }
+}
