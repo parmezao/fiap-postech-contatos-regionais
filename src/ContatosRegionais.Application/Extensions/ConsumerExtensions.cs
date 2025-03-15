88248203a96584e9f598c809c552e64b46620890
Wagner Cristiano Parmezão <wagner@parmex.com.br>
2025-03-15 18:43:29 -0300

    Add ContatosRegionais.Infra.Consumer project
    - Updated solution file to include `ContatosRegionais.Infra.Consumer` project with necessary configurations.
    - Enhanced `ContatosRegionais.Application.csproj` with new package references for MassTransit, OpenTelemetry, and Polly.
    - Introduced `ConsumerExtensions` class for dependency injection setup in `ConsumerExtensions.cs`.
    - Updated configuration files (`appsettings.Development.json` and `appsettings.json`) for RabbitMQ and database connection strings.
    - Created `ContatosRegionais.Infra.Consumer.csproj` targeting `net8.0` with required package references.
    - Modified `Program.cs` to register a hosted service `Worker` for background tasks.
    - Added `Worker.cs` class to log activity at regular intervals.
    - Created `launchSettings.json` for development environment configuration.

---
+++ src/ContatosRegionais.Application/Extensions/ConsumerExtensions.cs
@@ -0,0 +1,113 @@
+﻿using ContatosRegionais.Application.Consumers;
+using ContatosRegionais.Domain.Entities;
+using ContatosRegionais.Domain.Interfaces;
+using ContatosRegionais.Infra.Data.Context;
+using ContatosRegionais.Infra.Data.Repository;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Polly;
+
+namespace ContatosRegionais.Application.Extensions;
+
+public static class ConsumerExtensions
+{
+    public static void AddInjections(this IServiceCollection services, IConfiguration configuration)
+    {
+        //Data
+        services.AddDbContext<SqlServerDbContext>(options => options
+            .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        //Repo
+        services.AddScoped<IBaseRepository<Contato>, BaseRepository<Contato>>();
+
+        const string serviceName = "InfraConsumer";
+
+        services.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+
+                var rabbitMqHost = configuration["RabbitMQ:RABBITMQ_HOST"];
+                var rabbitMqUser = configuration["RabbitMQ:RABBITMQ_USER"] ?? string.Empty;
+                var rabbitMqPassword = configuration["RabbitMQ:RABBITMQ_PASSWORD"] ?? string.Empty;
+
+                //cfg.Host(new Uri("rabbitmq://localhost:5672/"), h =>
+                cfg.Host(rabbitMqHost, h =>
+                {
+                    h.Username(rabbitMqUser);
+                    h.Password(rabbitMqPassword);
+                });
+
+                // Configuração de fila específica para InsertContatoConsumer
+                cfg.ReceiveEndpoint("insert-contact-queue", e =>
+                {
+                    e.ConfigureConsumer<InsertContatoConsumer>(context);
+                });
+
+                // Configuração de fila específica para UpdateContatoConsumer
+                cfg.ReceiveEndpoint("update-contact-queue", e =>
+                {
+                    e.ConfigureConsumer<UpdateContatoConsumer>(context);
+                });
+
+                // Configuração de fila específica para DeleteContatoConsumer
+                cfg.ReceiveEndpoint("delete-contact-queue", e =>
+                {
+                    e.ConfigureConsumer<DeleteContatoConsumer>(context);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+
+            x.AddConsumer<InsertContatoConsumer>();
+            x.AddConsumer<UpdateContatoConsumer>();
+            x.AddConsumer<DeleteContatoConsumer>();
+        });
+
+
+
+        // Configuração do OpenTelemetry
+        services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(serviceName))
+            .WithTracing(tracing => tracing
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+            )
+            .WithMetrics(metrics =>
+            {
+                metrics
+                    .AddRuntimeInstrumentation()
+                    .AddProcessInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddPrometheusExporter();  // Exposição para Prometheus
+            });
+
+
+        // Configuração de Polly para RabbitMQ com retries
+        services.AddSingleton<IAsyncPolicy>(policy =>
+        {
+            return Policy.Handle<Exception>()
+                         .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        });
+
+        services.AddOpenTelemetry()
+        .ConfigureResource(resource => resource.AddService(serviceName))
+        .WithTracing(tracing => tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+        )
+        .WithMetrics(metrics =>
+        {
+            metrics
+                .AddRuntimeInstrumentation()
+                .AddProcessInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddPrometheusExporter();  // Exposição para Prometheus
+        });
+    }
+}
---
+++ src/ContatosRegionais.Infra.Consumer/Program.cs
@@ -0,0 +1,9 @@
+using ContatosRegionais.Infra.Consumer;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+
+
+var host = builder.Build();
+host.Run();
